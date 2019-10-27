00c21a03c55c56191e6982f06a3bbba82892a0f6
Mohammad Shoaei <mohammad.shoaei@outlook.com>
2019-10-27 19:18:44 +0330

    unique index for field "code"

--- bots/db.go
+++ bots/db.go
@@ -6,9 +6,11 @@ import (
 	"os"
 	"time"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/mongo/readpref"
+	"go.mongodb.org/mongo-driver/x/bsonx"
 )
 
 var (
@@ -19,7 +21,7 @@ var (
 
 func init() {
 	var err error
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
 	defer cancel()
 	client, err = mongo.Connect(ctx, options.Client().ApplyURI("mongodb://miner:password@localhost:27017/News?authSource=admin&compressors=disabled&gssapiServiceName=mongodb"))
 	if err != nil {
@@ -32,4 +34,27 @@ func init() {
 	}
 	db = client.Database("News")
 	collection = db.Collection("data")
+
+	c, err := collection.Indexes().List(ctx)
+	defer c.Close(ctx)
+	key := bson.M{}
+	exists := false
+	for c.Next(ctx) {
+		err = c.Decode(&key)
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Println(key)
+		if key["name"] == "code_1" {
+			exists = true
+		}
+	}
+	if !exists {
+		keys := bsonx.Doc{bsonx.Elem{Key: "code", Value: bsonx.Int32(1)}}
+		indexOpts := options.Index().SetUnique(true)
+		_, err := collection.Indexes().CreateOne(ctx, mongo.IndexModel{Keys: keys, Options: indexOpts})
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
 }
