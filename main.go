a576bf73726692f96eb98ce61c1e7e9f74fbf2ad
Mohammad Shoaei <mohammad.shoaei@outlook.com>
2019-12-19 01:09:47 +0330

    added some comment for clarification.

--- main.go
+++ main.go
@@ -1,13 +1,17 @@
 package main
 
 import (
-	"github.com/MShoaei/NewsMiner/bots"
 	"os/exec"
 	"time"
+
+	"github.com/MShoaei/NewsMiner/bots"
 )
 
 func main() {
-	exportCmds := make(chan *exec.Cmd, 5)
+	// number 10 is just that, A NUMBER!
+	// it can be changed but should at least be equal
+	// to the number of bots running in parallel!
+	exportCmds := make(chan *exec.Cmd, 10)
 
 	go bots.BBCExtract(exportCmds)
 	// go bots.YJCExtract(exportCmds)
@@ -15,8 +19,13 @@ func main() {
 	// go bots.TabnakExtract(exportCmds)
 	go bots.TasnimExtract(exportCmds)
 
+	// short sleep to make sure all the bots have started
+	// successfully before closing the channel.
 	time.Sleep(4 * time.Second)
+
+	// colsing the channel is needed because the bots.Log() function
+	// needs to know when to end the for loop and finish the execution.
 	close(exportCmds)
-	
+
 	bots.Log(exportCmds)
 }
